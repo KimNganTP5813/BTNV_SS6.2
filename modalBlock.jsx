280cc779f0c94e0a67052f188e4b0301d422769d
KimNganTP5813 <akinosora13@gmail.com>
2025-04-28 00:12:08 +0900

    Create modalBlock.jsx

---
+++ modalBlock.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function modalBlock() {
+  return (
+    <div className="overlay" hidden="">
+    <div className="modal-custom">
+      <div className="modal-title">
+        <h4>Cảnh báo</h4>
+        <i className="fa-solid fa-xmark" />
+      </div>
+      <div className="modal-body-custom">
+        <span>Bạn có chắc chắn muốn chặn tài khoản này?</span>
+      </div>
+      <div className="modal-footer-custom">
+        <button className="btn btn-light">Hủy</button>
+        <button className="btn btn-danger">Xác nhận</button>
+      </div>
+    </div>
+    </div>
+  )
+}
