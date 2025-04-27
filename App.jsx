1f95cb51a6f4f077830efc0822439bf49124498d
KimNganTP5813 <akinosora13@gmail.com>
2025-04-27 23:49:45 +0900

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+import StudentsManager from "./components/StudentsManager";
+
+function App() {
+  return (
+    <>
+      <StudentsManager/>
+    </>
+  );
+}
+
+export default App;
